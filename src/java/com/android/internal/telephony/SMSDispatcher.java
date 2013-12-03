c3ccb86ad6a0aabfc2474aa7ae37731c898a282b
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-12-02 21:14:10 -0600

    Revert "SMSDispatcher: Fix determining the defaultSmsApp for proxied messages"
    This reverts commit 56c3c130a42dfb537322ae186aebe8e6e5523f0a.

--- src/java/com/android/internal/telephony/SMSDispatcher.java
+++ src/java/com/android/internal/telephony/SMSDispatcher.java
@@ -716,17 +716,7 @@ public abstract class SMSDispatcher extends Handler {
 
         // Get calling app package name via UID from Binder call
         PackageManager pm = mContext.getPackageManager();
-        int callingUid = Binder.getCallingUid();
-        // Special case: We're being proxied by the telephony stack itself,
-        // so use the intent generator's UID if one exists
-        String[] packageNames;
-
-        if (callingUid == android.os.Process.PHONE_UID && sentIntent != null &&
-                sentIntent.getCreatorPackage() != null) {
-            packageNames = new String[] { sentIntent.getCreatorPackage() };
-        } else {
-            packageNames = pm.getPackagesForUid(callingUid);
-        }
+        String[] packageNames = pm.getPackagesForUid(Binder.getCallingUid());
 
         if (packageNames == null || packageNames.length == 0) {
             // Refuse to send SMS if we can't get the calling package name.
@@ -1131,17 +1121,7 @@ public abstract class SMSDispatcher extends Handler {
             PendingIntent deliveryIntent, String format) {
         // Get calling app package name via UID from Binder call
         PackageManager pm = mContext.getPackageManager();
-        int callingUid = Binder.getCallingUid();
-        // Special case: We're being proxied by the telephony stack itself,
-        // so use the intent generator's UID if one exists
-        String[] packageNames;
-
-        if (callingUid == android.os.Process.PHONE_UID && sentIntent != null &&
-                sentIntent.getCreatorPackage() != null) {
-            packageNames = new String[] { sentIntent.getCreatorPackage() };
-        } else {
-            packageNames = pm.getPackagesForUid(callingUid);
-        }
+        String[] packageNames = pm.getPackagesForUid(Binder.getCallingUid());
 
         // Get package info via packagemanager
         PackageInfo appInfo = null;
