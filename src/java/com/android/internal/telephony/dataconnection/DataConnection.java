8720090fe4c83019bf39594a86413f82fa4c4232
Harley Glossop <harleyglossop@gmail.com>
2014-12-01 03:21:42 +0000

    Legacy RIL requires radio technology equals CDMA or GSM
    With the latest update radio technology is set to indicate the type of network connected (GPRS, HSUPA, etc) and not just CDMA/GSM, breaking data connectivity for legacy RILs.
    
    Will only affect RIL Versions 4 or below for now, I don't know if later versions are also affected.
    
    Change-Id: I7062acf3299d67b3f62ce02b875e25a83c19fa62

--- src/java/com/android/internal/telephony/dataconnection/DataConnection.java
+++ src/java/com/android/internal/telephony/dataconnection/DataConnection.java
@@ -556,7 +556,9 @@ public final class DataConnection extends StateMachine {
         }
 
         mPhone.mCi.setupDataCall(
-                Integer.toString(cp.mRilRat + 2),
+                Integer.toString((mPhone.mCi.getRilVersion() > 4) ? cp.mRilRat + 2
+                        : ServiceState.isGsm(cp.mRilRat + 2) ? RILConstants.SETUP_DATA_TECH_GSM
+                        : RILConstants.SETUP_DATA_TECH_CDMA),
                 Integer.toString(cp.mProfileId),
                 mApnSetting.apn, mApnSetting.user, mApnSetting.password,
                 Integer.toString(authType),
