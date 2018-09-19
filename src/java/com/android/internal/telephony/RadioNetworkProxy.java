86c52122be657fbfaff4a4b5febe9762542e594a
faust93 <monumentum@gmail.com>
2018-09-19 14:26:54 +0800

    RIL: Fix manual network selection with old modem
     * Old modem sends and applies mccmnc+network type, but new aosp
       telephony uses only digits.
     * Manually cut or append the "+" and strings after that to work around.
    
    Change-Id: I1015e6c0d6afdc0c03d70e9491d49331703eddcc

--- src/java/com/android/internal/telephony/RadioNetworkProxy.java
+++ src/java/com/android/internal/telephony/RadioNetworkProxy.java
@@ -22,6 +22,7 @@ import android.annotation.NonNull;
 import android.os.AsyncResult;
 import android.os.Message;
 import android.os.RemoteException;
+import android.os.SystemProperties;
 import android.telephony.AccessNetworkConstants;
 import android.telephony.NetworkScanRequest;
 import android.telephony.RadioAccessSpecifier;
@@ -557,6 +558,13 @@ public class RadioNetworkProxy extends RadioServiceProxy {
     public void setNetworkSelectionModeManual(int serial, String operatorNumeric, int ran)
             throws RemoteException {
         if (isEmpty()) return;
+
+        boolean oldMncMccFormat = SystemProperties.getBoolean(
+                "ro.telephony.use_old_mnc_mcc_format", false);
+        if (oldMncMccFormat) {
+            operatorNumeric += "+";
+        }
+
         if (isAidl()) {
             mNetworkProxy.setNetworkSelectionModeManual(serial, operatorNumeric,
                     RILUtils.convertToHalAccessNetworkAidl(ran));
