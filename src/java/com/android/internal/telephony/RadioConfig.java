d3665e7b58946d7368381275f7df5d265e6f919f
Avinash Nalluri <anallu@codeaurora.org>
2021-06-02 21:52:51 -0700

    Check if RIL is supported
    Request for Radioconfig service only if RIL is supported.
    
    Change-Id: Ie5e337c88f78f4e25dbebffd547a81983ca8aa5b
    CRs-Fixed: 2961523

--- src/java/com/android/internal/telephony/RadioConfig.java
+++ src/java/com/android/internal/telephony/RadioConfig.java
@@ -37,6 +37,7 @@ import android.os.Message;
 import android.os.Registrant;
 import android.os.RemoteException;
 import android.os.ServiceManager;
+import android.os.SystemProperties;
 import android.os.Trace;
 import android.os.WorkSource;
 import android.telephony.TelephonyManager;
@@ -79,6 +80,9 @@ public class RadioConfig extends Handler {
     protected Registrant mSimultaneousCallingSupportStatusRegistrant;
 
     private boolean isMobileDataCapable(Context context) {
+        if(SystemProperties.getBoolean("ro.radio.noril", false)) {
+            return false;
+        }
         final TelephonyManager tm = context.getSystemService(TelephonyManager.class);
         return tm != null && tm.isDataCapable();
     }
