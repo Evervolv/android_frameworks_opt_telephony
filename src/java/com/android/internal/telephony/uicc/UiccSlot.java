74508ec3c34204c4199dd1fef6141bdffa38bf18
LuK1337 <priv.luk@gmail.com>
2023-11-01 21:40:05 +0100

    Make UiccSlot::getPrintableIccIds() skip null values
    collect() requires non-null values.
    
    Change-Id: Id5192b374e8ea608ce86bc3d73691d7569f92526

--- src/java/com/android/internal/telephony/uicc/UiccSlot.java
+++ src/java/com/android/internal/telephony/uicc/UiccSlot.java
@@ -661,6 +661,7 @@ public class UiccSlot extends Handler {
 
     private Map<Integer, String> getPrintableIccIds() {
         Map<Integer, String> printableIccIds = mIccIds.entrySet().stream()
+                .filter(e -> e.getValue() != null)
                 .collect(Collectors.toMap(Map.Entry::getKey,
                         e -> SubscriptionInfo.getPrintableId(e.getValue())));
         return printableIccIds;
